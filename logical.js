e0003605a9d72086a630ed4398bd115edc50a065
Yasin Arafat <yasinarafat2557@gmail.com>
2024-10-11 19:44:16 +0600

    logical operator And , Or

---
+++ logical.js
@@ -0,0 +1,13 @@
+
+
+// Logical And, Or operator
+
+const person = {
+    nameeee: "mehedy",
+    gender: "male",
+    isStudent: " "
+}
+
+console.log(person.isStudent && "He is student")
+
+console.log(person.name || "N/A")
