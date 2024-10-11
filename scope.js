0f79b184d52393b71b3740382f52df7fbc208800
Yasin Arafat <yasinarafat2557@gmail.com>
2024-10-11 17:33:18 +0600

    scope practice

---
+++ scope.js
@@ -0,0 +1,17 @@
+// let x = 34;
+
+// {
+//     console.log(x)
+//     let x2 = 50;
+// }
+// console.log(x2)
+
+
+
+const y = "something"
+console.log(y)
+{
+    console.log(y)
+    const y2 = "anything"
+}
+console.log(y2)
